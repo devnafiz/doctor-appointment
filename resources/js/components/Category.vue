850e10e0f3e082c83d72e282c0bbd971c67d6a9c
nafiz <nafiz.2011@yahoo.com>
2023-06-11 23:20:52 +0600

    update

--- resources/js/components/Category.vue
+++ resources/js/components/Category.vue
@@ -4,7 +4,7 @@ import  axios from 'axios'
 
 
 import {ref, onMounted, reactive } from 'vue'
-
+import { Form, Field, useResetForm } from 'vee-validate';
 
 const formValues = ref();
 const form = ref(null);
@@ -24,9 +24,23 @@ const getCategory=()=>{
 
 }
 
+const addCategory=()=>{
+	
+	editing.value =false;
+	 $('#categoryFormModal').modal('show'); 
+}
+
 const editCategory=(category)=>{
+    //console.log(category);
+    form.value.resetForm();
+    editing.value=true;
 	
      $('#categoryFormModal').modal('show'); 
+     formValues.value = {
+        id:category.id,
+        name:category.name
+
+     };
 
 }
 
@@ -34,6 +48,24 @@ onMounted(()=>{
     getCategory();
     //toastr.info('success');
 })
+
+const handleSubmit=(values,actions)=>{
+	
+	// console.log(actions);
+	if(editing.value){
+	updateCategory(values, actions)
+
+	}else{
+
+          createUser(values, actions);
+	}
+}
+
+const updateCategory=(values)=>{
+axios.put('http://127.0.0.1:8000/api/categories/')
+	
+
+}
 
 
 </script>
@@ -55,6 +87,7 @@ onMounted(()=>{
 			</div>
 </div>
 <div>
+   <button @click="addCategory" class="m-2 btn btn-primary">add Category</button>
 	<table class="table table-bordered">
        <thead>
 		<tr>
@@ -83,20 +116,22 @@ onMounted(()=>{
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="staticBackdropLabel">
-                        <span v-if="editing" >Edit User</span>
-                        <span  v-else>Add New User</span>
+                        <span v-if="editing" >Edit Category</span>
+                        <span  v-else>Add New Category</span>
                     </h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <Form  ref="form"  @submit="handleSubmit"  :initial-values="formValues">
+                <Form  ref="form"  @submit="handleSubmit"  :initial-values="formValues"  v-slot="{ errors }"> 
                     <div class="modal-body">
                         <div class="form-group">
                             <label for="name">Name</label>
-                            <Field  name="name" type="text" class="form-control"
-                                id="name"  placeholder="Enter full name" :class="{'is_invalid': errors.name}" />
-                            <span class="invalid-feedback">{{errors.name}}</span>
+                            <Field type="text" name="name" class="form-control"  id="name"
+
+                                placeholder="Enter full name" :class="{'is_invalid': errors.name}" 
+                            />
+                           
                         </div>
 
                         
